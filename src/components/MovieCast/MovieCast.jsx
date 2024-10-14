30b516dd4ac2a61ef69a0afd5cfd607c7ea3edf9
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-10-14 19:29:10 +0300

    Add default image and media rule fole mob device

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -25,6 +25,9 @@ export default function MovieCast() {
     getMovieCast();
   }, [movieId]);
 
+  const defaultImg =
+    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+
   return (
     <div>
       {isLoading && <Loader />}
@@ -36,23 +39,15 @@ export default function MovieCast() {
         <ul className={css.castList}>
           {cast.map((actor) => (
             <li key={actor.id} className={css.castItem}>
-              {actor.profile_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
-                  alt={actor.name || "Actor"}
-                  style={{ width: "100px", height: "150px" }}
-                />
-              ) : (
-                <p
-                  style={{
-                    width: "100px",
-                    height: "150px",
-                    textAlign: "center",
-                  }}
-                >
-                  Image not found
-                </p>
-              )}
+              <img
+                src={
+                  actor.profile_path
+                    ? `https://image.tmdb.org/t/p/w500${actor.profile_path}`
+                    : defaultImg
+                }
+                alt={actor.name || "Actor"}
+                style={{ width: "100px", height: "150px" }}
+              />
               <div>
                 {actor.name ? (
                   <p className={css.actorName}>{actor.name}</p>
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -56,6 +56,9 @@ export default function MovieDetailsPage() {
     );
   };
 
+  const defaultImg =
+    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+
   return (
     <div>
       <Link to={handleLinkGoBack} className={css.linkGoBack}>
@@ -68,7 +71,11 @@ export default function MovieDetailsPage() {
         <div className={css.movieDetailsContainer}>
           <img
             className={css.movieImage}
-            src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+            src={
+              movieDetails.poster_path
+                ? `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+                : defaultImg
+            }
             alt={movieDetails.title}
           />
           <div className={css.movieDetailsInfo}>
